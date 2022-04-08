aa8d193492632a0bfcb46a9516fa7138874324bf
Luis <lraulam.03@gmail.com>
2022-04-07 22:05:49 -0500

    Added animation for rolling dice

---
+++ Animations.h
@@ -0,0 +1,64 @@
+#include "raylib.h"
+
+typedef struct sprite_struct
+{
+    Texture2D texture;
+    Rectangle mask;
+    int frameCount;
+}Sprite;
+
+typedef struct sprite_graphics_struct
+{
+    Sprite sprite;
+    int currentFrame;
+    float deltaFrame;
+    float refreshRate;
+}SpriteGraphics;
+
+
+Sprite LoadSprite(const char *filename, int frameCount);
+SpriteGraphics LoadSpriteGraphics(const char *filename, int frameCount, float refresh);
+SpriteGraphics UpdateSpriteGraphic(SpriteGraphics g);
+int RollDice();
+
+
+int RollDice()
+{
+    int num = GetRandomValue(1, 6);
+    return num;
+}
+
+
+Sprite LoadSprite(const char *filename, int frameCount)
+{
+    Texture2D texture = LoadTexture(filename);
+    Sprite s = {
+            texture,
+            {0, 0, texture.width / frameCount, texture.height},
+            frameCount
+    };
+    return s;
+}
+
+SpriteGraphics LoadSpriteGraphics(const char *filename, int frameCount, float refresh)
+{
+    SpriteGraphics g = {
+            LoadSprite(filename, frameCount), 0, 0, refresh
+    };
+    return g;
+}
+
+SpriteGraphics UpdateSpriteGraphic(SpriteGraphics g)
+{
+    float elapsed = GetFrameTime();
+    g.deltaFrame += elapsed;
+
+    if(g.deltaFrame >= g.refreshRate)
+    {
+        g.deltaFrame = 0;
+        g.currentFrame = (g.currentFrame + 1) % g.sprite.frameCount;
+        g.sprite.mask.x = g.sprite.mask.width * g.currentFrame;
+    }
+
+    return g;
+}
